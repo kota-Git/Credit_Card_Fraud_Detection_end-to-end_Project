c2f945c059c2bd68e7fe40a71eca03bb61f801d2
Laxman <lakshmanteja9@gmail.com>
2024-11-24 21:34:35 +0530

    folder_structure_added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name= "demo_project",
+    version="0.0.0",
+    author="Laxman",
+    author_mail="lakshmanteja9@gmail.com",
+    description="ML_Demo_Project",
+    packages=find_packages()
+)
