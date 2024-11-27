e2f12ca22dab6468b82f2c184e930468670d202e
Laxman <lakshmanteja9@gmail.com>
2024-11-27 23:38:03 +0530

    constants_file_was_with_config

---
+++ Demo_project/constants/__init__.py
@@ -0,0 +1,35 @@
+import os
+from datetime import date
+
+DATABASE_NAME="demo_project_DB"
+
+COLLECTION_NAME="fraud_data"
+
+MONGODB_URL_KEY="MONGODB_URL"
+
+PIPELINE_NAME:str ="Demo_project"  # not a src file  
+
+ARTIFICAT_DIR:str="artificat"
+
+MODEL_FILE_NAME="model.pkl"
+
+TARGET_COLUMN="default payment next month"
+
+PREPROCESSING_OBJECT_FILE_NAME="preprocessing.pkl"
+
+
+FILE_NAME:str="default of credit card data.xls"
+
+TRAIN_FILE_NAME:str="train.xls"
+
+TEST_FILE_NAME:str="test.xls"
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "fraud_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
---
+++ demo.py
@@ -0,0 +1,5 @@
+from Demo_project.constants import DATABASE_NAME, COLLECTION_NAME
+
+print(DATABASE_NAME)
+
+print(COLLECTION_NAME)
