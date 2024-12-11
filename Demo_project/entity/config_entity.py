c0d20a6486718558a588fd3beaa34923b90e74dd
Laxman <lakshmanteja9@gmail.com>
2024-12-11 13:36:26 +0530

    constant_updated_for_data_transormation

--- Demo_project/entity/config_entity.py
+++ Demo_project/entity/config_entity.py
@@ -31,3 +31,6 @@ class DataValidationConfig:
     drift_report_file_path: str = os.path.join(data_validation_dir, DATA_VALIDATION_DRIFT_REPORT_DIR,
                                                DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
     
+
+
+
