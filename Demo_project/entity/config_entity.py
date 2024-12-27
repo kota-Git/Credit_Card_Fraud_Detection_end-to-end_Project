078f6cfd1fc182e0e55f0b098bcc531e5fa32c47
Laxman <lakshmanteja9@gmail.com>
2024-12-27 22:04:33 +0530

    entity_added_for_prediction_pipeline

--- Demo_project/entity/config_entity.py
+++ Demo_project/entity/config_entity.py
@@ -68,3 +68,10 @@ class ModelEvaluationConfig:
 class ModelPusherConfig:
     bucket_name: str = MODEL_BUCKET_NAME
     s3_model_key_path: str = MODEL_FILE_NAME
+
+
+
+@dataclass
+class CreditCardPredictorConfig:
+    model_file_path: str = MODEL_FILE_NAME
+    model_bucket_name: str = MODEL_BUCKET_NAME
